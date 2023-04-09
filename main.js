5be8a14f2f097a555fb7d682a1618f36514de118
sloth-89 <restvoice2020@gmail.com>
2023-04-10 01:33:56 +0900

    invitation

---
+++ main.js
@@ -0,0 +1,12 @@
+// let mainText = document.querySelector(".date")
+
+// window.addEventListener('scroll', function(){
+//     let value = window.scrollY;
+//     console.log("scrollY", value);
+
+//     if(value>900){
+//         mainText.style.animation = "deopacity 1s forwards";
+//     } else {
+//         mainText.style.animation = "opacity 2s"
+//     }
+// });
